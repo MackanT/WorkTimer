72f6223cb96ee0b0e924992fe3c98b1513271462
MackanT <mackan.toftas@live.com>
2025-06-16 18:23:31 +0200

    ad temp fix to avoid issues with expired PAT:s

--- devops.py
+++ devops.py
@@ -11,10 +11,18 @@ class DevOpsClient:
 
     def connect(self):
         # Create a connection to the Azure DevOps organization
-        credentials = BasicAuthentication("", self.personal_access_token)
-        self.connection = Connection(base_url=self.organization_url, creds=credentials)
-
-        self.wit_client = self.get_work_item_tracking_client()
+        try:
+            credentials = BasicAuthentication("", self.personal_access_token)
+            self.connection = Connection(
+                base_url=self.organization_url, creds=credentials
+            )
+            self.wit_client = self.get_work_item_tracking_client()
+        except AzureDevOpsServiceError as e:
+            if "expired" in str(e).lower():
+                print("Personal Access Token has expired. Please renew it.")
+                return -1
+            else:
+                print(f"Failed to connect to Azure DevOps: {e}")
 
     def get_work_item_tracking_client(self):
         if not self.connection:
