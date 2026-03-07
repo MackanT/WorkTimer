aa3787181645102e24d6bd069cb76dba5e14fee7
MackanT <mackan.toftas@live.com>
2026-03-07 22:03:50 +0100

    rm old v1 (v4) code from project

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-"""WorkTimer - A helper program to keep time of time spent on different tasks, with built in DevOps integration."""
-
-__version__ = "0.1.0"
-__author__ = "Marcus Toftås"
-__email__ = "mackan.toftas@live.com"
